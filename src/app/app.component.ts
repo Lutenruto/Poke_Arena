5a05858ac3036121a1dcb84cc7d5de18556aa405
Luis <pereiravazluis@gmail.com>
2021-07-21 10:12:18 +0200

    :white_check_mark: :sparkles: Add begin of project tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {PokemonService} from "./services/pokemon.service";
+import {Pokemon} from "./models/Pokemon";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Poke_Arena';
+    pokemons: Array<any> = [];
+
+    constructor(private pokemonService: PokemonService){}
+
+    ngOnInit(){
+        this.pokemonService.getAll().subscribe( (res:any) => {
+            for(let i = 0; i < res.results.length; i++){
+                let urlSplit = res.results[i].url.trim().split("/");
+                res.results[i].id = urlSplit[6];
+            }
+            this.pokemons = res.results;
+        })
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import {PokemonService} from "./services/pokemon.service";
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    PokemonService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
